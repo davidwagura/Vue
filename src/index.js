b53ee2b62fd5809bce467c9960784bede664c502
davidwagura <davidwagura83@gmail.com>
2023-03-22 16:21:23 +0300

    made some changes on the codes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 new Vue({
   el: '#app',
   data: {
-    newItem: '',
+    first_name: '',
+    second_name: '',
+    subject_math: 0,
+    subject_english: 0,
+    subject_kiswahili: 0,
     items: []
   },
+  computed: {
+   average() {
+     let avg = 0;
+     // let sum = v1+v2+v3
+     let sum = parseInt(this.subject_math) + parseInt(this.subject_english) + parseInt(this.subject_kiswahili);
+     avg = Math.ceil(sum/3);
+     return avg;
+   }
+  },
   methods: {
-  	changeMessage(){
-      this.items.push(this.newItem)
-      this.newItem = '';
+  	addtext(){
+      // console.log(this.first_name);
+      // console.log(this.second_name);
+      console.log(this.subject_math);
+      console.log(this.subject_english);
+      console.log(this.subject_kiswahili);
+      console.log(this.average);
   	}
   }
 })
