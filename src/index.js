d434a1ab72372c3be542c118ef970b1a58fbce4c
davidwagura <davidwagura83@gmail.com>
2023-03-23 14:55:38 +0300

    modified index.js

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ new Vue({
     subject_math: 0,
     subject_english: 0,
     subject_kiswahili: 0,
-    items: []
+    students: [],
+    student:''
   },
   computed: {
    average() {
      let avg = 0;
-     // let sum = v1+v2+v3
      let sum = parseInt(this.subject_math) + parseInt(this.subject_english) + parseInt(this.subject_kiswahili);
      avg = Math.ceil(sum/3);
      return avg;
@@ -19,12 +19,29 @@ new Vue({
   },
   methods: {
   	addtext(){
-      // console.log(this.first_name);
-      // console.log(this.second_name);
-      console.log(this.subject_math);
-      console.log(this.subject_english);
-      console.log(this.subject_kiswahili);
-      console.log(this.average);
-  	}
+      const student = {
+        firstname: this.first_name,
+        secondname: this.second_name,
+        mathematics: this.subject_math,
+        english: this.subject_english,
+        kiswahili: this.subject_kiswahili,
+        average: this.average
+      }
+      this.students.push(student)
+
+      // reset data
+      this.resetData();
+
+
+  	},
+
+    resetData(){
+      console.log('resetting')
+      this.first_name = ''
+      this.second_name = ''
+      this.subject_math = ''
+      this.subject_english = ''
+      this.subject_kiswahili = ''
+    }
   }
 })
